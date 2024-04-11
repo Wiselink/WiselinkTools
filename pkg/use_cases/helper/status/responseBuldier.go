0b2aedac142a54a03576acfa72c5211d3fe88196
sebastian demello <sebarray@gmail..com>
2024-04-11 11:13:30 -0300

    new builder response

--- pkg/use_cases/helper/status/responseBuldier.go
+++ pkg/use_cases/helper/status/responseBuldier.go
@@ -20,3 +20,16 @@ func ResponseBuilder(status int, message string, data interface{}) ([]byte, erro
 	return marshalResponse, nil
 
 }
+
+func ResponseStaNdard(s response.Status) response.Response {
+	return response.Response{
+		Resp: &s,
+	}
+}
+func ResponseCustom(status int, message string, data interface{}) response.Response {
+	return response.Response{
+		Status:  status,
+		Message: message,
+		Data:    data,
+	}
+}
