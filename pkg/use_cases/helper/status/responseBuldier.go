0040fe9b598f87b878435990d12ec87e5c1a6134
sebarray <sebarray98@gmail.com>
2024-05-02 11:06:39 -0300

    add readme

--- pkg/use_cases/helper/status/responseBuldier.go
+++ pkg/use_cases/helper/status/responseBuldier.go
@@ -21,15 +21,18 @@ func ResponseBuilder(status int, message string, data interface{}) ([]byte, erro
 
 }
 
-func ResponseStaNdard(s response.Status) response.Response {
+func ResponseStaNdard(s response.Status, err error, siteError string) response.Response {
 	return response.Response{
-		Resp: &s,
+		Resp:  &s,
+		Error: err,
 	}
 }
-func ResponseCustom(status int, message string, data interface{}) response.Response {
+func ResponseCustom(status int, message string, data interface{}, err error, siteError string) response.Response {
 	return response.Response{
 		Status:  status,
 		Message: message,
 		Data:    data,
+		Error:   err,
+		Site:    siteError,
 	}
 }
